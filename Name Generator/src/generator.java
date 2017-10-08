cd32c567984cb29002189fc734ad81bf83608b62
Exostrike <olivercrosbiehiggs@googlemail.com>
2017-10-08 17:20:41 +0100

    bug fix and relative paths

--- Name Generator/src/generator.java
+++ Name Generator/src/generator.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class generator {
 	  public static void main(String[] args) throws IOException {
 		  
-		  List<String> prefix = Files.readAllLines(Paths.get("C:\\Users\\Oliver\\workspace\\Name Generator\\src\\barsoom prefix.txt"));
-		  List<String> suffix = Files.readAllLines(Paths.get("C:\\Users\\Oliver\\workspace\\Name Generator\\src\\barsoom suffix.txt"));
+		  List<String> prefix = Files.readAllLines(Paths.get(".\\barsoom prefix.txt"));
+		  List<String> suffix = Files.readAllLines(Paths.get(".\\barsoom suffix.txt"));
 		  Random randomizer = new Random();
 		  List<String> list = new ArrayList<String>();
 		  
@@ -19,6 +19,7 @@ public class generator {
 	        for(count =0; count <= n-1; count++)
 	        {	        		   
 		    String prefix2 = prefix.get(randomizer.nextInt(prefix.size()));
+		    String prefix3 = prefix.get(randomizer.nextInt(prefix.size()));
 		    String suffix2 = suffix.get(randomizer.nextInt(suffix.size()));		    		      
 	        Random ran = new Random();
 	        int C = ran.nextInt(5);
@@ -28,7 +29,7 @@ public class generator {
 	         }else if( C>0 && C<3 ){
 	        	 list.add(prefix2 + suffix2+ " ");
 	         }else if( C>2 && C<6 ){
-	        	 list.add(prefix2 + " " + prefix2 + suffix2+ " ");
+	        	 list.add(prefix2 + " " + prefix3 + suffix2+ " ");
 	         	}
 	        }
 	        
